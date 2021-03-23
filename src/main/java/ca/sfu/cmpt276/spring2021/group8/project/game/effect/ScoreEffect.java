a8e44d44827eede154f37ccef7f0ad2fdb7426bb
djennedy <darren_jennedy@sfu,ca>
2021-03-22 23:31:18 -0700

    Changed gameEffect and gameResult to interfaces

--- src/main/java/ca/sfu/cmpt276/spring2021/group8/project/game/effect/ScoreEffect.java
+++ src/main/java/ca/sfu/cmpt276/spring2021/group8/project/game/effect/ScoreEffect.java
@@ -3,7 +3,7 @@ package ca.sfu.cmpt276.spring2021.group8.project.game.effect;
 /**
  * ScoreEffect is a GameEffect that contains a score change
  */
-public class ScoreEffect extends GameEffect {
+public class ScoreEffect implements GameEffect {
     public final int score;
 
     ScoreEffect(int score) {
