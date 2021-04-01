54b442275dd0a9e94a7922cef112d4f26f4e4a1a
djennedy <darren_jennedy@sfu,ca>
2021-03-31 20:41:27 -0700

    fixed paths of resources

--- src/main/java/ca/sfu/cmpt276/spring2021/group8/project/ImageLoader.java
+++ src/main/java/ca/sfu/cmpt276/spring2021/group8/project/ImageLoader.java
@@ -11,10 +11,10 @@ import java.io.IOException;
  */
 public class ImageLoader {
     // Player Images
-    public static BufferedImage playerup = ImageLoader.loadImage("src/resources/Images/playerup.png");
-    public static BufferedImage playerdown = ImageLoader.loadImage("src/resources/Images/playerdown.png");
-    public static BufferedImage playerleft = ImageLoader.loadImage("src/resources/Images/playerright.png");
-    public static BufferedImage playerright = ImageLoader.loadImage("src/resources/Images/playerleft.png");
+    public static BufferedImage playerup = ImageLoader.loadImage("src/main/resources/Images/playerup.png");
+    public static BufferedImage playerdown = ImageLoader.loadImage("src/main/resources/Images/playerdown.png");
+    public static BufferedImage playerleft = ImageLoader.loadImage("src/main/resources/Images/playerright.png");
+    public static BufferedImage playerright = ImageLoader.loadImage("src/main/resources/Images/playerleft.png");
 
     public static Image p1 = playerup.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
     public static Image p2 = playerdown.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
@@ -22,10 +22,10 @@ public class ImageLoader {
     public static Image p4 = playerright.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
 
     // Enemy Images
-    public static BufferedImage enemyup = ImageLoader.loadImage("src/resources/Images/enemyup.png");
-    public static BufferedImage enemydown = ImageLoader.loadImage("src/resources/Images/enemydown.png");
-    public static BufferedImage enemyleft = ImageLoader.loadImage("src/resources/Images/enemyleft.png");
-    public static BufferedImage enemyright = ImageLoader.loadImage("src/resources/Images/enemyright.png");
+    public static BufferedImage enemyup = ImageLoader.loadImage("src/main/resources/Images/enemyup.png");
+    public static BufferedImage enemydown = ImageLoader.loadImage("src/main/resources/Images/enemydown.png");
+    public static BufferedImage enemyleft = ImageLoader.loadImage("src/main/resources/Images/enemyleft.png");
+    public static BufferedImage enemyright = ImageLoader.loadImage("src/main/resources/Images/enemyright.png");
 
     public static Image e1 = enemyup.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
     public static Image e2 = enemydown.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
@@ -33,26 +33,26 @@ public class ImageLoader {
     public static Image e4 = enemyleft.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
 
     // Regular Reward Images
-    public static BufferedImage rewardImg = ImageLoader.loadImage("src/resources/Images/Pnew.png");
+    public static BufferedImage rewardImg = ImageLoader.loadImage("src/main/resources/Images/Pnew.png");
     public static Image rr = rewardImg.getScaledInstance(47, 47, Image.SCALE_DEFAULT);
 
     // Bonus Images
-    public static BufferedImage bonusImg = ImageLoader.loadImage("src/resources/Images/Anew.png");
+    public static BufferedImage bonusImg = ImageLoader.loadImage("src/main/resources/Images/Anew.png");
     public static Image bon = bonusImg.getScaledInstance(47, 47, Image.SCALE_DEFAULT);
 
     // Punishment Images
-    public static BufferedImage punishmentImg = ImageLoader.loadImage("src/resources/Images/Fnew.png");
+    public static BufferedImage punishmentImg = ImageLoader.loadImage("src/main/resources/Images/Fnew.png");
     public static Image pun = punishmentImg.getScaledInstance(47, 47, Image.SCALE_DEFAULT);
 
     // Barrier Images
-    public static BufferedImage barrierImg = ImageLoader.loadImage("src/resources/Images/barrier2.jpg");
+    public static BufferedImage barrierImg = ImageLoader.loadImage("src/main/resources/Images/barrier2.jpg");
     public static Image bar = barrierImg.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
 
     // Maze Images
-    public static BufferedImage wallImg = ImageLoader.loadImage("src/resources/Images/wall.jpg");
-    public static BufferedImage entranceImg = ImageLoader.loadImage("src/resources/Images/entrance.jpg");
-    public static BufferedImage exitClosedImg = ImageLoader.loadImage("src/resources/Images/exitclosed.png");
-    public static BufferedImage exitOpenImg = ImageLoader.loadImage("src/resources/Images/exitopen.png");
+    public static BufferedImage wallImg = ImageLoader.loadImage("src/main/resources/Images/wall.jpg");
+    public static BufferedImage entranceImg = ImageLoader.loadImage("src/main/resources/Images/entrance.jpg");
+    public static BufferedImage exitClosedImg = ImageLoader.loadImage("src/main/resources/Images/exitclosed.png");
+    public static BufferedImage exitOpenImg = ImageLoader.loadImage("src/main/resources/Images/exitopen.png");
 
     public static Image w = wallImg.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
     public static Image entr = entranceImg.getScaledInstance(49, 49, Image.SCALE_DEFAULT);
